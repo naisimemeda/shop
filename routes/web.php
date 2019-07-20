9231e9720bf569dd80c23464246237e42e93b70b
vagrant <vagrant@homestead>
2019-07-20 11:33:19 +0000

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
 });
