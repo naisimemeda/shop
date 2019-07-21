acbec1f0c6c391828386f996d7d7bf535930ccc0
vagrant <vagrant@homestead>
2019-07-21 07:12:18 +0000

    用户界面 - 商品详情页

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -40,4 +40,12 @@ class ProductsController extends Controller
             ],
         ]);
     }
+
+    public function show(Product $product){
+        if (!$product->on_sale) {
+            throw new \Exception('商品未上架');
+        }
+
+        return view('products.show', ['product' => $product]);
+    }
 }
