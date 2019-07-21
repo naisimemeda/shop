351365fa6f237901c156d3251376441da16aee84
vagrant <vagrant@homestead>
2019-07-21 07:23:22 +0000

    用户界面 - 商品收藏

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,6 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     Route::get('products', 'ProductsController@index')->name('products.index');
     Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+    Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+    Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 });
