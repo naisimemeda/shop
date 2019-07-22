e278a52311a94b23f2f550fc4bae56565b20a18c
vagrant <vagrant@homestead>
2019-07-22 03:07:38 +0000

    创建订单

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -95,4 +95,20 @@ class Order extends Model
             return false;
         }
     }
+
+    public function decreaseStock($amount){
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        $this->increment('stock', $amount);
+    }
+
 }
