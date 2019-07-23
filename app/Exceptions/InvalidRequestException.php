ecf71758abafd69f2d7ea83d0471865365fdb27d
vagrant <vagrant@homestead>
2019-07-23 03:23:38 +0000

    评价商品

--- app/Exceptions/InvalidRequestException.php
+++ app/Exceptions/InvalidRequestException.php
@@ -15,7 +15,6 @@ class InvalidRequestException extends Exception
     public function render(Request $request)
     {
         if ($request->expectsJson()) {
-            // json() 方法第二个参数就是 Http 返回码
             return response()->json(['msg' => $this->message], $this->code);
         }
 
